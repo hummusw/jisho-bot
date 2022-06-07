a3df64605e3d425cf5a35a5d34afb65567a74d08
Thomas Wang <hummusw@gmail.com>
2022-06-07 16:06:46 -0400

    Allow passing in token filename with command line argument

--- jisho-bot.py
+++ jisho-bot.py
@@ -1,13 +1,22 @@
+import sys
+
 import discord
 from discord.ext import commands
 # from discord_slash import SlashCommand
 
-# Read token from file
-token_file = open('token-test.txt', 'r')  # fixme testing
-token = token_file.read().strip()
+# Default token file location
+token_filename = 'token.txt'
 
-# Set up bot, slash commands
-bot = commands.Bot(command_prefix="$jisho ")  # fixme testing
-# slash = SlashCommand(bot, override_type=True)
-bot.load_extension("jisho-bot-cog")
-bot.run(token)
+if __name__ == '__main__':
+    # Allow default token file location to be overriden through via command line argument
+    if len(sys.argv) >= 2:
+        token_filename = sys.argv[1]
+
+    # Read token from file
+    token = open(token_filename, 'r').read().strip()
+
+    # Set up bot and run
+    bot = commands.Bot(command_prefix="!jisho ")
+    # slash = SlashCommand(bot, override_type=True)
+    bot.load_extension("jisho-bot-cog")
+    bot.run(token)
